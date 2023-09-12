e8619dec3306e7fbc39f81108b4fbe2722b82407
Alandersonjs <limaalanderson@gmail.com>
2023-09-12 16:45:07 -0300

    Descrição das alterações

---
+++ adoptopia/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Adoptopia - Login</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <header>
+    <h1>Adoptopia</h1>
+    <!-- Links para login e cadastro -->
+    <nav>
+      <a href="login.php">Login</a>
+      <a href="cadastro.php">Cadastro</a>
+    </nav>
+  </header>
+
+  <main>
+    <!-- Formulário de login -->
+    <form>
+      <h2>Faça Login</h2>
+      <!-- Campos do formulário -->
+      <input type="email" name="email" placeholder="E-mail" required>
+      <input type="password" name="senha" placeholder="Senha" required>
+      <button type="submit">Entrar</button>
+    </form>
+  </main>
+
+  <footer>
+    <!-- Informações sobre a importância da adoção responsável e cuidados com os animais -->
+    <p>Adoptopia - Todos os direitos reservados</p>
+  </footer>
+</body>
+</html>
